33188d12f46944f71a3dbcb7b0a4daa9988e773e
dntoll <daniel@familjentoll.se>
2014-12-25 17:16:02 +0100

    remove dbug

--- src/PongView.cc
+++ src/PongView.cc
@@ -7,28 +7,31 @@ using namespace std;
 PongView::PongView(const PongModel &m, ili9341 &lcd) : model(m), screen(lcd) {
 	ovx = 0;
 	ovy = 0;
+
+	screen.clearScreen();
 }
 
 void PongView::draw() {
 	//It seems that the joystick has some blind zones, jumping from one interval to another
 		//calibration collects all values that are achieved
 	//cout << "frame draw" << endl;
-	screen.clearScreen();
+
 	/*bool foundNewValues = false;
 	int fx = stick.getX();
 	int fy = stick.getY();*/
 
 	const Ball &b = model.getBall();
+	//old ball
+	screen.fillBox(ovx, ovy, 10, 10, 0, 0, 0);
+
 	int vx = map(b.getX() * 1024, 0, 1024, 310, 10);
 	int vy = map(b.getY() * 1024, 0, 1024, 230, 10);
 	screen.fillBox(0, 0, 320, 10, 255, 255, 255);
 	screen.fillBox(0, 230, 320, 10, 255, 255, 255);
 	screen.fillBox(0, 0, 10, 240, 255, 255, 255);
-	screen.fillBox(230, 0, 10, 240, 255, 255, 255);
+	screen.fillBox(310, 0, 10, 240, 255, 255, 255);
 
-
-	//cout << "fill box " << ovx << " " << ovy << endl;
-	screen.fillBox(ovx, ovy, 10, 10, 0, 0, 0);
+	//Ball
 	screen.fillBox(vx, vy, 10, 10, 255, 255, 0);
 	ovx = vx;
 	ovy = vy;

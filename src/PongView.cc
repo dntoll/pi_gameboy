16c241e96664ce53707acab1e1e71fd7868facb0
dntoll <daniel@familjentoll.se>
2014-12-25 16:41:24 +0100

    new screen class

--- src/PongView.cc
+++ src/PongView.cc
@@ -16,7 +16,7 @@ void PongView::draw() {
 	const Ball &b = model.getBall();
 	int vx = map(b.getX() * 1024, 0, 1024, 230, 10);
 	int vy = map(b.getY() * 1024, 0, 1024, 310, 10);
-	
+	cout << "frame draw" << endl;
 	screen.fillBox(ovx, ovy, 10, 10, 0, 0, 0);
 	screen.fillBox(vx, vy, 10, 10, 255, 255, 0);
 	ovx = vx;
